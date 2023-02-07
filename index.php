afe21796c59e9e456acf180e7f51c6b68decd099
Achille Kouassi <101454995+achillekouassi@users.noreply.github.com>
2023-02-07 19:48:56 +0100

    Update index.php

--- index.php
+++ index.php
@@ -1,47 +1,44 @@
 <!DOCTYPE html>
 <html>
+
 <head>
-  <title>Échiquier PHP</title>
-  <style>
-    .chess-board {
-      display: flex;
-      flex-wrap: wrap;
-      width: 200px;
-    }
-    .chess-square {
-      width: 25%;
-      height: 25%;
-      background-color: #fff;
-    }
-    .chess-square:nth-child(odd) {
-      background-color: #000;
-    }
-  </style>
+  <title></title>
+  <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
 </head>
+
 <body>
-  <h1>Créer un échiquier PHP</h1>
-  <form action="<?php echo $_SERVER['PHP_SELF']; ?>" method="post">
-    <label for="rows">Nombre de lignes :</label>
-    <input type="number" id="rows" name="rows">
-    <br><br>
-    <label for="columns">Nombre de colonnes :</label>
-    <input type="number" id="columns" name="columns">
-    <br><br>
-    <input type="submit" value="Créer échiquier">
+  <form action="" method="post">
+    <label for="">nombre colonne & ligne </label>
+    <input type="text" name="number" placeholder="entrer un nombre"><br>
+    <input type="submit" value="Submit" name="submit">
   </form>
-  <br><br>
-  <?php
-    if ($_SERVER["REQUEST_METHOD"] == "POST") {
-      $rows = $_POST["rows"];
-      $columns = $_POST["columns"];
-      echo '<div class="chess-board">';
-      for ($i = 0; $i < $rows; $i++) {
-        for ($j = 0; $j < $columns; $j++) {
-          echo '<div class="chess-square"></div>';
+  <br>
+  <br>
+
+  <table width="400px" cellspacing="0px" cellpadding="0px" border="1px">
+
+    <?php
+//recuperation du nombre entreer par l'utlisateur pour generer le damier
+    if (isset($_POST['submit'])) {
+
+      $number = $_POST['number'];
+
+      for ($row = 1; $row <= $number; $row++) {
+        echo "<tr>";
+        for ($col = 1; $col <= $number; $col++) {
+          $total = $row + $col;
+          if ($total % 2 == 0) {
+            echo "<td height=60px width=60px bgcolor= white></td>";
+          } else {
+            echo "<td height=60px width=60px bgcolor=black></td>";
+          }
         }
+        echo "</tr>";
       }
-      echo '</div>';
     }
-  ?>
+    ?>
+  </table>
+
 </body>
+
 </html>
